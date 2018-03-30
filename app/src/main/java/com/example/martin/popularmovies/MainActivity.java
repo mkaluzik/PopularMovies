b0e70e992efa0ac66b8881e6b42fe9fa48e843b8
mkaluzik <mkaluzik@gmail.com>
2018-03-30 21:15:23 +0200

    30.3. 21:15
    Permission requested, Settings menu in place

--- app/src/main/java/com/example/martin/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/martin/popularmovies/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
